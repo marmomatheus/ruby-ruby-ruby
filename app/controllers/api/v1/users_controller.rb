37b4b41f67fb4e16e81515445aca9354784a0fbe
Matheus Marmo <matheusmarmo55@gmail.com>
2019-05-13 09:41:18 -0300

    basic api

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,45 @@
+module Api
+  module V1
+    class UsersController < ApplicationController
+        
+      def index
+          users = User.all
+          render json: { data:users } ,status: :ok
+      end
+
+      def show
+				user = User.find(params[:id])
+				render json: { data:user },status: :ok
+      end
+      
+      def create
+				user = User.new(user_params)
+				if user.save
+					render json: { data:user },status: :ok
+				else
+					render json: { data:user.errors },status: :unprocessable_entity
+				end
+      end
+
+      def destroy
+				user = User.find(params[:id])
+				user.destroy
+				render json: { data:user },status: :ok
+      end
+      
+			def update
+				user = User.find(params[:id])
+				if user.update_attributes(user_params)
+					render json: { data:user },status: :ok
+				else
+					render json: { data:user.errors },status: :unprocessable_entity
+				end
+			end
+      
+      private
+			def user_params
+				params.permit(:name, :email)
+			end
+    end
+  end
+end
